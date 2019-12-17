3117e95e1d03937df1f582815cb1fa02810c8646
Manguet <benjamin.manguet@gmail.com>
2019-12-17 20:29:40 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Afin de récupérer le projet:
+
+## Etape 1:
+* git clone https://github.com/Manguet/Hackaton.git
+* composer install
+* yarn install
+
+## Etape 2:
+* Lancer yarn encore dev --watch
+* Lancer symfony server:start
+
+## Etape 3:
+* Vérifier que tout s'affiche correctement.
+
+
+# Fonctionnalités implantées:
+
+* Bootstrap
+* Formulaires Bootstrap
+* Font Awesome free
+* Un service de slugger que l'on peut implanter directement dans une méthode!
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="app_")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("", name="")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
