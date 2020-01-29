44771932e735ee5c4de46c8f1073d96de64c5960
Manguet <benjamin.manguet@gmail.com>
2020-01-29 11:17:38 +0100

    StartNav

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("", name="")
+     * @Route("", name="index")
      */
     public function index()
     {
